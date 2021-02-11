2afa3545d0b4036c796b2db6fec812b9befde81c
David Lawson <39196767+linuxlawson@users.noreply.github.com>
2021-02-10 19:44:28 -0500

    non resizable
    and shortened code

--- login.py
+++ login.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # Login Window
-# David Lawson
 
 
 try:
@@ -14,7 +13,7 @@ root.title("\tLogin")
 root.geometry("272x152")
 root.option_add("*Font", "TkDefaultFont 9")
 root.columnconfigure(0, weight=1)
-
+root.resizable(0,0)
 
 #Main Frame
 mainframe = tk.Frame(root, highlightcolor="#BFBFBF", 
@@ -30,17 +29,9 @@ password = tk.StringVar()
 
 def check_entry():
     if username.get() == 'smitty' and password.get() == 'pencil':
-        print ("Access Granted")
-        status_label.grid_remove()
-        stat1 = tk.Label(root, text=" Access: Granted ", anchor='w', 
-                        fg="#7F7F7F", bd=1, relief='sunken')
-        stat1.grid(row=4, column=0, sticky='ew', padx=5, pady=2)
+        status_label.config(text=" Access: Granted")
     else:
-        print ("Access Denied")
-        status_label.grid_remove()
-        stat2 = tk.Label(root, text=" Access: Denied ", anchor='w', 
-                        fg="#7F7F7F", bd=1, relief='sunken')
-        stat2.grid(row=4, column=0, sticky='ew', padx=5, pady=2)
+        status_label.config(text=" Access: Denied")
 
 
 #username
@@ -56,6 +47,7 @@ pass_label.grid(row=1, column=0, sticky='w', padx=6, pady=8)
 pass_entry = tk.Entry(mainframe, textvariable=password, show="*")
 pass_entry.grid(row=1, column=1, sticky='e', padx=6, pady=8)
 
+
 status_label = tk.Label(root, text=" Access: Pending", anchor='w', 
                         fg="#7F7F7F", bd=1, relief='sunken')
 status_label.grid(row=4, column=0, sticky='ew', padx=4, pady=2)
@@ -69,7 +61,6 @@ cancel_button.bind("<Return>", (lambda event: root.destroy()))
 login_button = tk.Button(mainframe, text="Login ", command=check_entry)
 login_button.grid(row=3, column=1, sticky='e', padx=6, pady=8)
 login_button.bind("<Return>", (lambda event: check_entry()))
-
 
 
 root.protocol("WM_DELETE_WINDOW")
