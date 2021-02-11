f5eb30f1aa866e35d4357cf8ace1625377536162
David Lawson <39196767+linuxlawson@users.noreply.github.com>
2021-02-10 19:46:58 -0500

    non resizable

--- login5.py
+++ login5.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # Login Window #5
-# David Lawson
 
 
 try:
     import Tkinter as tk
 except:
     import tkinter as tk
- 
+
 
 root = tk.Tk()
-root.title("\tLogin")
+root.title("\tUser Login")
 root.geometry("263x181")
 root.option_add("*Font", "TkDefaultFont 9")
-
+root.resizable(0,0)
 
 #Main Frame
 mainframe = tk.Frame(root, highlightcolor="#1A1A1A",
@@ -29,18 +28,9 @@ password = tk.StringVar()
 
 def check_entry():
     if username.get() == 'smitty' and password.get() == 'pencil':
-        print ("Access Granted")
-        status_label = tk.Label(mainframe, text="\nAccess Granted\n", anchor='n', 
-                        bg="#000000", fg="#15DD15", bd=1, relief='sunken')
-        status_label.grid(row=3, column=0, columnspan=2, sticky='ew', padx=4, pady=6)
-        
+        status_label.config(text="\nAccess Granted\n", fg="#15DD15")
     else:
-        print ("Access Denied")
-        status_label = tk.Label(mainframe, 
-                                text="##############\n#    Access Denied    #\n##############", 
-                                anchor='n', 
-                                bg="#000000", fg="#15DD15", bd=1, relief='sunken')
-        status_label.grid(row=3, column=0, columnspan=2, sticky='ew', padx=4, pady=6)
+        status_label.config(text="##############\n#    Access Denied    #\n##############", fg="#15DD15")
 
 
 #username
@@ -60,7 +50,6 @@ status_label = tk.Label(mainframe, text="\nAuthorized Users Only\n", anchor='n',
                         bg="#000000", fg="#E5E5E5", bd=1, relief='sunken')
 status_label.grid(row=3, column=0, columnspan=2, sticky='ew', padx=4, pady=6)
 
-
 #buttons
 cancel_button = tk.Button(mainframe, text="Cancel", command=root.destroy)
 cancel_button.grid(row=2, column=1, sticky='w', padx=4, pady=8)
@@ -69,7 +58,6 @@ cancel_button.bind("<Return>", (lambda event: root.destroy()))
 login_button = tk.Button(mainframe, text="Login ", command=check_entry)
 login_button.grid(row=2, column=1, sticky='e', padx=4, pady=8)
 login_button.bind("<Return>", (lambda event: check_entry()))
-
 
 
 root.protocol("WM_DELETE_WINDOW")
