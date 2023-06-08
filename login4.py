8524bac9e9600819c95271c98b89e0d401f566b7
David <linuslagoo@yahoo.com>
2023-06-08 01:29:06 -0400

    py3-updated

--- login4.py
+++ login4.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # Login Window #4
 
 
@@ -9,17 +9,22 @@ except:
 
 
 root = tk.Tk()
-root.title("\tUser Login")
-root.geometry("263x188")
+root.title("User Login")
+root.geometry("262x188")
 root.option_add("*Font", "TkDefaultFont 9")
 root.resizable(0,0)
+
+class MainApp:
+	def __init__(self, master):
+		self.master = master
+		
 
 #Main Frame
 mainframe = tk.Frame(root, highlightcolor="#1A1A1A",
                     highlightbackground="#7F7F7F", 
                     highlightthickness=1, 
                     relief='flat')
-mainframe.grid(sticky='nsew', padx=5, pady=2)
+mainframe.grid(sticky='nsew', padx=4, pady=2)
 
 
 username = tk.StringVar()
@@ -28,13 +33,11 @@ password = tk.StringVar()
 
 def check_entry():
     if username.get() == 'smitty' and password.get() == 'pencil':
-        print ("Access Granted")
         status_label = tk.Label(mainframe, text="Permission Granted", anchor='w', 
                                 fg="#008000", bd=0, relief='sunken')
         status_label.grid(row=4, column=1, columnspan=2, sticky='w', padx=4, pady=4)
         
     else:
-        print ("Access Denied")
         status_label = tk.Label(mainframe, text="Permission Denied\t", anchor='w', 
                                 fg="#A52A2A", bd=0, relief='sunken')
         status_label.grid(row=4, column=1, columnspan=2, sticky='w', padx=4, pady=4)
@@ -66,14 +69,14 @@ status_label.grid(row=4, column=0, sticky='e', padx=4, pady=4)
 
 #buttons
 cancel_button = tk.Button(mainframe, text="Cancel", command=root.destroy)
-cancel_button.grid(row=3, column=1, sticky='w', padx=4, pady=8)
+cancel_button.grid(row=3, column=1, sticky='w', padx=4, pady=4)
 cancel_button.bind("<Return>", (lambda event: root.destroy()))
 
 login_button = tk.Button(mainframe, text="Login ", command=check_entry)
-login_button.grid(row=3, column=1, sticky='e', padx=4, pady=8)
+login_button.grid(row=3, column=1, sticky='e', padx=4, pady=4)
 login_button.bind("<Return>", (lambda event: check_entry()))
 
 
 
-root.protocol("WM_DELETE_WINDOW")
+app = MainApp(root)
 root.mainloop()
