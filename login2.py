c24b8e379c9aae904c24aa44cae9ccfdb77ed695
David <linuslagoo@yahoo.com>
2023-07-19 00:09:47 -0400

    login two

--- login2.py
+++ login2.py
@@ -4,51 +4,52 @@
 import tkinter as tk
  
 root = tk.Tk()
-root.title("\tLogin")
+root.title("Login")
 root.geometry("272x164")
 root.option_add("*Font", "TkDefaultFont 9")
 root.columnconfigure(0, weight=1)
 root.resizable(0,0)
 
+#Main Frame
 mainframe = tk.LabelFrame(root, text="User Login", bd=1, relief='solid')
-mainframe.grid(row=0, column=0, sticky='n', padx=2, pady=2)
+mainframe.grid(column=0, row=0, padx=2, pady=2, sticky='n')
 
 username = tk.StringVar()
 password = tk.StringVar()
 
 
-def check_entry():
+def check_entry(event=None):
     if username.get() == 'smitty' and password.get() == 'pencil':
         status_label.config(text=" Access: Granted")
     else:
         status_label.config(text=" Access: Denied")
 
-
 #username
 user_label = tk.Label(mainframe, text="Username:")
-user_label.grid(row=0, column=0, sticky='w', padx=6, pady=8)
+user_label.grid(column=0, row=0, padx=6, pady=8, sticky='w')
 user_entry = tk.Entry(mainframe, textvariable=username)
-user_entry.grid(row=0, column=1, sticky='e', padx=6, pady=8)
+user_entry.grid(column=1, row=0, padx=6, pady=8, sticky='e')
 user_entry.focus_set()
 
 #password
 pass_label = tk.Label(mainframe, text="Password:")
-pass_label.grid(row=1, column=0, sticky='w', padx=6, pady=8)
+pass_label.grid(column=0, row=1, padx=6, pady=8, sticky='w')
 pass_entry = tk.Entry(mainframe, textvariable=password, show="*")
-pass_entry.grid(row=1, column=1, sticky='e', padx=6, pady=8)
+pass_entry.grid(column=1, row=1, padx=6, pady=8, sticky='e')
+pass_entry.bind('<Return>', check_entry)
 
 status_label = tk.Label(mainframe, text=" Access: Pending", anchor='w', 
                         fg="#7F7F7F", bd=1, relief='sunken')
-status_label.grid(row=4, column=0, columnspan=2, sticky='ew', padx=0, pady=0)
+status_label.grid(column=0, row=4, padx=0, pady=0, columnspan=2, sticky='ew')
 
 
 #buttons
 cancel_button = tk.Button(mainframe, text="Cancel", command=root.destroy)
-cancel_button.grid(row=3, column=1, sticky='w', padx=6, pady=10)
+cancel_button.grid(column=1, row=3, padx=6, pady=8, sticky='w')
 cancel_button.bind("<Return>", (lambda event: root.destroy()))
 
 login_button = tk.Button(mainframe, text="Login ", command=check_entry)
-login_button.grid(row=3, column=1, sticky='e', padx=6, pady=10)
+login_button.grid(column=1, row=3, padx=6, pady=8, sticky='e')
 login_button.bind("<Return>", (lambda event: check_entry()))
 
 
